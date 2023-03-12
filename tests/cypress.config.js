ac1de387ed55d658e9ef612bc6f6fe5a3a0db56e
Gabriel de Araujo <gabrielaraujovni@gmail.com>
2023-03-12 17:22:57 -0300

    Testando a action

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
--- tests/cypress/e2e/onAir.cy.js
+++ tests/cypress/e2e/onAir.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
+describe('Web app deve estar online', () => {
   it('passes', () => {
     cy.visit('/')
+
+    cy.title().should('eq', 'Samurai Barbershop by QAninja')
   })
 })
